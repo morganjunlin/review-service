132793d5a2b054701ff0a8c2ac08596b31fee6d1
Morgan Lin <morganjunlin@gmail.com>
2020-03-20 19:54:24 -0700

    heroku changes

--- database/index.js
+++ database/index.js
@@ -5,7 +5,7 @@ const seeder = require('./seed.js');
 
 require('dotenv').config();
 
-let DB_URL = (process.env.NODE_ENV === 'development') ? 'mongodb://localhost/FEC_Nordstrom' : `mongodb+srv://dbUser:${process.env.DB_PW}@cluster0-awanb.mongodb.net/test?retryWrites=true&w=majority`
+let DB_URL = (process.env.NODE_ENV === 'development') ? 'mongodb://localhost/fec-nordstrom' : `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@nordstrom-ux-cluster-hokx3.mongodb.net/fec-nordstrom?retryWrites=true&w=majority`
 
 
 mongoose.connect(DB_URL, {useNewUrlParser: true});
